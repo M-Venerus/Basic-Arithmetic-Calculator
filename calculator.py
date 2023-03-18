a4b8438edefa3771e7cf49575a84d7fe213b1571
M-Venerus <mikey.motorist@outlook.com>
2023-03-18 09:43:45 -0400

    Add TODOs

--- calculator.py
+++ calculator.py
@@ -1,1 +1,31 @@
-print("Weclome to the Basic Arithmetic Calculator App")
+print("Weclome to the Basic Arithmetic Calculator Application!\n")
+
+# TODO Create dictionary for operators and their defined functions
+
+
+
+# TODO Define main function for the two numbers and their result. Will call 
+# the dictionary functions.
+
+
+
+# TODO Create a print statement that prints the first number, the operator, the second
+# number, and what their result is.
+
+
+
+# TODO Type 'y' to continue calculating with 'the result', or type 'n' to start a new calculation.
+# This means if they enter 'y' that they will be using the previous result as the first 
+# number and be prompted to pick an operation again. If they type 'n', a while loop will 
+# loop back to the beginning and start from scratch (The program will always be functioning)
+
+# Some code I pre-wrote below that needs to find its place.
+num_1 = input("What is the first number?: ")
+print('''+
+-
+*
+/''')
+operator = input("Pick an operation: ")
+num_2 = input("What's the next number?: ")
+
+
